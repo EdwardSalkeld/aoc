3aa90b582ab51599932dbdfb06c8c4aaae474754
Ed Salkeld <edward.salkeld@brightnetwork.co.uk>
2025-12-03 11:38:27 +0000

    day 3 part 2

--- 2025/03/src/solve.py
+++ 2025/03/src/solve.py
@@ -2,17 +2,35 @@ def solve(part: int = 1) -> int:
     raw_input = read_input()
     if part == 1:
         return sum([calculate_joltage(x) for x in raw_input])
-    answer = 0
+    if part == 2:
+        return sum([calculate_joltage_variable(x, 12) for x in raw_input])
     return 0
 
 
 def calculate_joltage(line: str) -> int:
-    first_number_candidates = list(line[:-1])
+    first_number_candidates = line[:-1]
     first = max(first_number_candidates)
     first_ix = first_number_candidates.index(first)
-    second_number_candidates = list(line[first_ix + 1 :])
+    second_number_candidates = line[first_ix + 1 :]
     second = max(second_number_candidates)
     return 10 * int(first) + int(second)
+
+
+def calculate_joltage_variable(line: str, required: int) -> int:
+    joltages = []
+    start_ix = 0
+    while required > 0:
+        required -= 1
+        if required == 0:
+            candidates = line[start_ix:]
+        else:
+            candidates = line[start_ix:-required]
+        # print(f"L {line} C {candidates} F {start_ix} E {-required}")
+        selected = max(candidates)
+        start_ix += candidates.index(selected) + 1
+        multiplier = 10 ** (required)
+        joltages.append(int(selected) * multiplier)
+    return sum(joltages)
 
 
 def main() -> None:
--- 2025/03/test/test.py
+++ 2025/03/test/test.py
@@ -16,10 +16,24 @@ class TestExample(unittest.TestCase):
         answer = solve.solve()
         self.assertEqual(answer, 357)
         answer_part2 = solve.solve(part=2)
-        self.assertEqual(answer_part2, 0)
+        self.assertEqual(answer_part2, 3121910778619)
 
     def test_calculate_joltage(self):
         self.assertEqual(solve.calculate_joltage("987654321111111"), 98)
         self.assertEqual(solve.calculate_joltage("811111111111119"), 89)
         self.assertEqual(solve.calculate_joltage("234234234234278"), 78)
         self.assertEqual(solve.calculate_joltage("818181911112111"), 92)
+
+    def test_calculate_joltage_12(self):
+        self.assertEqual(
+            solve.calculate_joltage_variable("987654321111111", 12), 987654321111
+        )
+        self.assertEqual(
+            solve.calculate_joltage_variable("811111111111119", 12), 811111111119
+        )
+        self.assertEqual(
+            solve.calculate_joltage_variable("234234234234278", 12), 434234234278
+        )
+        self.assertEqual(
+            solve.calculate_joltage_variable("818181911112111", 12), 888911112111
+        )
