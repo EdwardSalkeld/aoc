ae67f7b0223d3242185bb53b3ad1f9fbdbfbef9c
Ed Salkeld <edward.salkeld@brightnetwork.co.uk>
2025-12-06 13:02:33 +0000

    template tweaks

--- 2025/template/test/test.py
+++ 2025/template/test/test.py
@@ -2,11 +2,18 @@ import unittest
 from unittest.mock import patch
 from src import solve
 
+EXAMPLE = """
+
+"""
+
+ANSWER1 = 0
+ANSWER2 = 0
+
 
 class TestExample(unittest.TestCase):
-    @patch("src.solve.read_input", return_value="1,9-12,20,22,1010, 462149")
+    @patch("src.solve.read_input", return_value=EXAMPLE)
     def test_final_answer(self, _mock):
         answer = solve.solve()
-        self.assertEqual(answer, 0)
+        self.assertEqual(answer, ANSWER1)
         answer_part2 = solve.solve(part=2)
-        self.assertEqual(answer_part2, 0)
+        self.assertEqual(answer_part2, ANSWER2)
