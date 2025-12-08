5e796ee5d73a8483aac72a6610357d1ceb9f9c41
Ed Salkeld <edward.salkeld@brightnetwork.co.uk>
2025-12-08 13:01:35 +0000

    day 8 part 1

---
+++ 2025/08/src/solve.py
@@ -0,0 +1,109 @@
+import math
+
+
+def solve(part: int = 1, connections=1000) -> int:
+    raw_input = read_input()
+    boxes = expand_input(raw_input)
+    distance_matrix = calculate_distances(list(boxes))
+    connection_sets = create_links(distance_matrix, connections)
+    group_count, group_coverage, product = count_groups(connection_sets)
+    if part == 1:
+        print(f"groups = {group_count} + ({len(boxes)} - {group_coverage})")
+        return product
+    answer = 0
+    return 0
+
+
+def calculate_distances(
+    boxes: list[tuple[int, int, int]],
+) -> dict[float, tuple[int, int, int]]:
+    distance_map = {}
+    while boxes:
+        box = boxes.pop()
+        # print(f"Handling {box}")
+        for other in boxes:
+            distance = math.dist(box, other)
+            if distance in distance_map:
+                raise ValueError("Duplicate distance found")
+            distance_map[distance] = (box, other)
+            # print(f"Between {box} and {other} is {distance}")
+    print(f"Calculated {len(distance_map)} distances")
+    return distance_map
+
+
+def create_links(distance_map: dict[float, tuple[int, int, int]], connections: int):
+    connection_sets = {}
+    distances = sorted(distance_map.keys())
+    connections_made = 0
+    checked = 0
+    while connections_made < connections:
+        pair = distance_map[distances[checked]]
+        checked += 1
+        if pair[0] not in connection_sets and pair[1] not in connection_sets:
+            # neither are connected. easy. add both ways
+            connection_sets[pair[0]] = set()
+            connection_sets[pair[1]] = connection_sets[pair[0]]
+        elif pair[0] not in connection_sets:
+            # one not connected. use other's
+            connection_sets[pair[0]] = connection_sets[pair[1]]
+        elif pair[1] not in connection_sets:
+            # same, but other way round
+            connection_sets[pair[1]] = connection_sets[pair[0]]
+        elif pair[1] in connection_sets[pair[0]]:
+            # these two are already connected. move along
+            # print("Already connected (but not skipping):", pair)
+            pass
+        else:
+            # both connected, but not to each other. need to merge sets
+            # print("Merge!")
+            set_to_merge = connection_sets[pair[1]]
+            for item in set_to_merge:
+                connection_sets[item] = connection_sets[pair[0]]
+            connection_sets[pair[0]].update(set_to_merge)
+        connection_sets[pair[0]].add(pair[0])
+        connection_sets[pair[0]].add(pair[1])
+        # print(f"Linking {pair[0]} and {pair[1]}")
+        connections_made += 1
+
+    return connection_sets
+
+
+def count_groups(
+    connection_sets: dict[tuple[int, int, int], set],
+) -> tuple[int, int, int]:
+    all_groups = set([frozenset(x) for x in connection_sets.values()])
+    group_count = len(all_groups)
+    group_coverage = sum(len(g) for g in all_groups)
+    print(f"Lens of all groups: {[len(g) for g in all_groups]}")
+    print(f"Found {group_count} groups covering {group_coverage} boxes")
+    largest_three_groups = sorted([len(g) for g in all_groups])[-3:]
+    print(f"Largest three groups: {largest_three_groups}")
+    product = (
+        largest_three_groups[0] * largest_three_groups[1] * largest_three_groups[2]
+    )
+    print(f"Product of largest three groups: {product}")
+    return group_count, group_coverage, product
+
+
+def expand_input(raw_input: list[str]) -> list[tuple[int, int, int]]:
+    boxes = []
+    for line in raw_input:
+        x, y, z = map(int, line.split(","))
+        boxes.append((x, y, z))
+    return boxes
+
+
+def main() -> None:
+    part_one = solve()
+    print(f"Part One: {part_one}")  # 68112
+    # part_two = solve(part=2)
+    # print(f"Part Two: {part_two}")
+
+
+def read_input() -> list[str]:
+    with open("./input", "r") as f:
+        return f.read().strip().splitlines()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2025/08/test/test.py
@@ -0,0 +1,72 @@
+import unittest
+import math
+from unittest.mock import patch
+from src import solve
+
+EXAMPLE = """
+162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689
+""".strip().splitlines()
+
+ANSWER1 = 40
+ANSWER2 = 0
+
+
+class TestExample(unittest.TestCase):
+    @patch("src.solve.read_input", return_value=EXAMPLE)
+    def test_final_answer(self, _mock):
+        answer = solve.solve(connections=10)
+        self.assertEqual(answer, ANSWER1)
+        answer_part2 = solve.solve(part=2, connections=10)
+        self.assertEqual(answer_part2, ANSWER2)
+
+    def test_expand_input(self):
+        text = """
+        1,2,3
+        4,5,6
+        7,8,9
+        """.strip().splitlines()
+        expected = [
+            (1, 2, 3),
+            (4, 5, 6),
+            (7, 8, 9),
+        ]
+        result = solve.expand_input(text)
+        self.assertEqual(result, expected)
+
+    def test_calculate_distances(self):
+        boxes = [
+            (0, 0, 0),
+            (3, 4, 0),
+            (0, 8, 6),
+        ]
+        result = solve.calculate_distances(boxes)
+        expected_distances = {
+            5.0: ((3, 4, 0), (0, 0, 0)),
+            10.0: ((0, 8, 6), (0, 0, 0)),
+            math.sqrt(61): ((0, 8, 6), (3, 4, 0)),
+        }
+        print(f"{result}")
+        for dist in expected_distances:
+            print("Checking distance:", dist)
+
+            self.assertIn(dist, result)
+            self.assertEqual(result[dist], expected_distances[dist])
