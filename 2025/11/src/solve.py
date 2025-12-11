954ff290eaa1b4978a3cd04cd5c5ed5801eeeac9
Ed Salkeld <edward.salkeld@brightnetwork.co.uk>
2025-12-11 14:40:12 +0000

    day 11 part 2 - tidy up a bit

--- 2025/11/src/solve.py
+++ 2025/11/src/solve.py
@@ -30,47 +30,7 @@ def solve(part: int = 1) -> int:
             set(),
             target="fft",
         )
-        print(f"Eligible from svr to fft: {len(eligible_from_svr_to_fft)}/{node_count}")
-        eligible_from_fft_to_dac = set()
-        walk_graph_collect_eligible(
-            node_lookups, ["fft"], eligible_from_fft_to_dac, set(), target="dac"
-        )
-        print(f"Eligible from fft to dac: {len(eligible_from_fft_to_dac)}/{node_count}")
-
-        eligible_from_fft_to_out = set()
-        walk_graph_collect_eligible(
-            node_lookups, ["fft"], eligible_from_fft_to_out, set(), target="out"
-        )
-        print(f"Eligible from fft to out: {len(eligible_from_fft_to_out)}/{node_count}")
-
         # magic knowledge from above: the only route is src->fft->dac->out
-        limit_nodes = eligible_from_svr_to_fft.union(
-            eligible_from_fft_to_dac, eligible_from_dac_to_out
-        )
-        print(f"Total limited nodes: {len(limit_nodes)}/{node_count}")
-        limited_node_lookups = {
-            k: v for k, v in node_lookups.items() if k in limit_nodes
-        }
-        paths1 = set()
-        walk_graph(limited_node_lookups, ["svr"], paths1, target="fft")
-        print(f"Total valid paths svr-fft: {len(paths1)}")
-        # for p in paths1:
-        #     print(p)
-
-        paths2 = set()
-        walk_graph(limited_node_lookups, ["fft"], paths2, target="dac")
-        print(f"Total valid paths fft-dac: {len(paths2)}")
-        # for p in paths2:
-        #     print(p)
-
-        paths3 = set()
-        walk_graph(limited_node_lookups, ["dac"], paths3, target="out")
-        print(f"Total valid paths dac-out: {len(paths3)}")
-        # for p in paths3:
-        #     print(p)
-
-        print(f"Produt of paths: {len(paths1)} * {len(paths2)} * {len(paths3)}")
-        print(f"= {len(paths1) * len(paths2) * len(paths3)}")
 
         set1 = set()
         set2 = set()
@@ -79,10 +39,8 @@ def solve(part: int = 1) -> int:
         walk_graph2(node_lookups, ["fft"], set2, set(), set(), target="dac")
         walk_graph2(node_lookups, ["dac"], set3, set(), set(), target="out")
         print(f"Set lens: {len(set1)} {len(set2)} {len(set3)}")
-        print(f"Product of sets: {len(set1) * len(set2) * len(set3)}")
 
-        return len(paths3)
-    answer = 0
+        return len(set1) * len(set2) * len(set3)
     return 0
 
 
@@ -137,33 +95,6 @@ def walk_graph2(
     if start_node not in path_nodes:
         bad_nodes.add(start_node)
     # print(f"GOOD: {len(path_nodes)} BAD: {len(bad_nodes)}")
-
-
-# def walk_graph_2(
-#     node_lookups: dict[str, list[str]],
-#     current_path: list[str],
-#     paths: set[str],
-#     target="out",
-# ):
-#     start_node = current_path[-1]
-#     node_connections = node_lookups.get(start_node, None)
-#     if node_connections is None:
-#         return
-#     for connection in node_connections:
-#         if connection == target:
-#             if "dac" in current_path and "fft" in current_path:
-#                 path_str = ",".join(current_path + [connection])
-#                 # print(f"Found path: {path_str}")
-#                 paths.add(path_str)
-#             # else:
-#             #     print(f"Discard path: {current_path}")
-#         elif connection == "out":
-#             continue  # skip out if its not the target
-#
-#         else:
-#             walk_graph_2(
-#                 node_lookups, current_path + [connection], paths, target=target
-#             )
 
 
 def walk_graph_collect_eligible(
